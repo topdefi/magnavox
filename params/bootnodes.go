bb291bf4aecafde54e2153cbb201472da0861680
Topdefi <94203269+topdefi@users.noreply.github.com>
2022-07-22 13:02:21 -0700

    Initial Upload

---
+++ params/bootnodes.go
@@ -0,0 +1,40 @@
+// Copyright 2015 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package params
+
+import "github.com/ethereum/go-ethereum/common"
+
+// MainnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
+// the main Ethereum network.
+var MainnetBootnodes = []string{
+	// Ethereum Foundation Go Bootnodes//TODO
+	"",
+	"",
+}
+
+// TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
+var TestnetBootnodes = []string{
+	"",
+	"",
+}
+
+// KnownDNSNetwork returns the address of a public DNS-based node list for the given
+// genesis hash and protocol. See https://github.com/ethereum/discv4-dns-lists for more
+// information.
+func KnownDNSNetwork(genesis common.Hash, protocol string) string {
+	return ""
+}
